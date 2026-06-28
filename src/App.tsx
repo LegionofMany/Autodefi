ad0582e20b5387d2c29a0a14b6c65507217137bf
Voltaire <mkolbaford@gmail.com>
2026-06-28 15:11:18 -0600

    Use vote-specific layout for Vote tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Shell } from './components/Shell';
+import { VoteLayout } from './components/VoteLayout';
 import { LenderPool } from './pages/LenderPool';
 import { ModulePage } from './pages/ModulePage';
 import { VoteDetails } from './pages/VoteDetails';
@@ -7,9 +8,17 @@ import { VoteDetails } from './pages/VoteDetails';
 export default function App() {
   const [activeView, setActiveView] = useState('vote');
 
+  if (activeView === 'vote') {
+    return (
+      <VoteLayout activeView={activeView} onNavigate={setActiveView}>
+        <VoteDetails />
+      </VoteLayout>
+    );
+  }
+
   return (
     <Shell activeView={activeView} onNavigate={setActiveView}>
-      {activeView === 'lender-pool' ? <LenderPool /> : activeView === 'vote' ? <VoteDetails /> : <ModulePage id={activeView} />}
+      {activeView === 'lender-pool' ? <LenderPool /> : <ModulePage id={activeView} />}
     </Shell>
   );
 }
