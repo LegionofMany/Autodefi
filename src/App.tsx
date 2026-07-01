2909cc4f45132880077aa0393c30831f01eb1d67
Voltaire <mkolbaford@gmail.com>
2026-06-30 20:33:09 -0600

    Route default app to locked dealer portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import { Shell } from './components/Shell';
+import DealerPortal from './components/dealer/DealerPortal';
 import { LenderPool } from './pages/LenderPool';
 import { ModulePage } from './pages/ModulePage';
 
 export default function App() {
-  const [activeView, setActiveView] = useState('lender-pool');
+  const [activeView, setActiveView] = useState('dealer-portal');
+
+  if (activeView === 'dealer-portal') {
+    return <DealerPortal />;
+  }
+
   return (
     <Shell activeView={activeView} onNavigate={setActiveView}>
       {activeView === 'lender-pool' ? <LenderPool /> : <ModulePage id={activeView} />}
