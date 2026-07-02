8c4325844694ca68396063f75565cd3d6dc23a87
Voltaire <mkolbaford@gmail.com>
2026-07-01 19:34:51 -0600

    Route loan servicing dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { Shell } from './components/Shell';
 import { LenderPool } from './pages/LenderPool';
+import { LoanServicing } from './pages/LoanServicing';
 import { ModulePage } from './pages/ModulePage';
 
 export default function App() {
-  const [activeView, setActiveView] = useState('lender-pool');
+  const [activeView, setActiveView] = useState('loan-servicing');
   return (
     <Shell activeView={activeView} onNavigate={setActiveView}>
-      {activeView === 'lender-pool' ? <LenderPool /> : <ModulePage id={activeView} />}
+      {activeView === 'lender-pool' ? <LenderPool /> : activeView === 'loan-servicing' ? <LoanServicing /> : <ModulePage id={activeView} />}
     </Shell>
   );
 }
