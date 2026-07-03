98b662060caf44fc1178ce108f09f6637e5dcd43
Voltaire <mkolbaford@gmail.com>
2026-07-02 20:22:07 -0600

    Route treasury nav to locked management center

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import { Shell } from './components/Shell';
 import { LenderPool } from './pages/LenderPool';
 import { ModulePage } from './pages/ModulePage';
+import { TreasuryManagementCenter } from './pages/TreasuryManagementCenter';
 
 export default function App() {
-  const [activeView, setActiveView] = useState('lender-pool');
+  const [activeView, setActiveView] = useState('treasury');
+
+  if (activeView === 'treasury') {
+    return <TreasuryManagementCenter onBack={() => setActiveView('dashboard')} />;
+  }
+
   return (
     <Shell activeView={activeView} onNavigate={setActiveView}>
       {activeView === 'lender-pool' ? <LenderPool /> : <ModulePage id={activeView} />}
