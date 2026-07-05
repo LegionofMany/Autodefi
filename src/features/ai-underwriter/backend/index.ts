fccb4cf5889b180828cfdf35c021bc8a02744c7b
Voltaire <mkolbaford@gmail.com>
2026-07-05 15:04:06 -0600

    Add backend brain exports

---
+++ src/features/ai-underwriter/backend/index.ts
@@ -0,0 +1,7 @@
+export * from './auditLogService';
+export * from './brainTypes';
+export * from './governanceWorkflow';
+export * from './outcomeLearningService';
+export * from './runBackendBrain';
+export * from './v1DecisionEngine';
+export * from './v2ShadowAuditEngine';
