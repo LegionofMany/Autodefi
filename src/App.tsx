63dde0d2e655a954a165803bbbb9103dcb48d4a3
Voltaire <mkolbaford@gmail.com>
2026-07-05 12:53:28 -0600

    Wire AI Underwriter command center page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react';
 import { Shell } from './components/Shell';
 import { LenderPool } from './pages/LenderPool';
 import { ModulePage } from './pages/ModulePage';
+import { AIUnderwriterCommandCenter } from './pages/AIUnderwriterCommandCenter';
 
 export default function App() {
   const [activeView, setActiveView] = useState('lender-pool');
+
+  let content = <ModulePage id={activeView} />;
+  if (activeView === 'lender-pool') content = <LenderPool />;
+  if (activeView === 'ai-underwriter-v2') content = <AIUnderwriterCommandCenter />;
+
   return (
     <Shell activeView={activeView} onNavigate={setActiveView}>
-      {activeView === 'lender-pool' ? <LenderPool /> : <ModulePage id={activeView} />}
+      {content}
     </Shell>
   );
 }
