7695d6aec825d9f963b005f90827270a3f5c86f2
Voltaire <mkolbaford@gmail.com>
2026-07-05 15:25:40 -0600

    Add V2 client exports

---
+++ src/features/ai-underwriter/client/index.ts
@@ -0,0 +1,8 @@
+export {
+  createBackendBrainGovernancePacket,
+  createPersistedV1Decision,
+  readBackendBrainSnapshot,
+  runBackendBrainAudit,
+  runBackendBrainClientDemo
+} from './v2BackendBrainClient';
+export type { V2BackendBrainClientResult } from './v2BackendBrainClient';
